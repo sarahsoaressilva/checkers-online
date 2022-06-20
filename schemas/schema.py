239427fba5bb530d947f6a6c6f9b8e1ba537b0c7
Sarah Soares <contatodev.sarah@gmail.com>
2022-06-20 19:15:52 -0300

    py: Cria tabela e modelo tabuleiro

--- schemas/schema.py
+++ schemas/schema.py
@@ -2,10 +2,18 @@ from typing import Union, Optional
 from pydantic import BaseModel
 
 # Modelagem da Tabela.
-
-
 class User(BaseModel):
     user_id: Optional[int]
     username: Optional[str]
     email: str
     password: str
+
+# Modelagem da Tabela Tabuleiro.
+class Tabuleiro(BaseModel):
+    tab_id: Optional[int]
+    nome: str
+    descricao: str
+    valor: float
+    img: str
+    plano_id: Optional[int]
+
